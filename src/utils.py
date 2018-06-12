689cdd1fdadfa5beac7292a98013912b5f7592a6
Simon Egersand <s.egersand@gmail.com>
2018-06-12 22:03:54 +0200

    Allow blob as image file type

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_IMAGE_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 
-def allowed_file_type(filename):
+def allowed_file_type(filename: str) -> bool:
+    if filename == 'blob':
+        return True
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_IMAGE_EXTENSIONS
